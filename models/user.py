1d5712f4244046bfadfafc7ed92a3b0416402a29
Boris <boris.thereis@gmail.com>
2024-09-09 20:40:35 +0500

    refactor User model and add migration

--- models/user.py
+++ models/user.py
@@ -8,12 +8,20 @@ class UserStatus(BaseTimeStampedModel):
     status_name = Column(String(25), nullable=False, unique=True)
 
 
-class User(BaseTimeStampedModel):
-    __tablename__ = 'user'
+class AbstractBaseUser(BaseTimeStampedModel):
+    __abstract__ = True
+    password = Column(String)
+    last_login = Column(DateTime, nullable=True)
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
+    status_id = Column(ForeignKey('user_status.id', name='user_status_id_fkey'), nullable=False)
+
+
+class User(AbstractBaseUser):
+    __tablename__ = 'user'
     date_of_birth = Column(Date)
-    status = Column(ForeignKey('user_status.id'), nullable=False)
     signup_datetime = Column(DateTime, nullable=False)
 
+    def __repr__(self) -> str:
+        return f'User, email={self.email}, status={self.status_id}'
